06095130ec836f555bfacedee588395b86abe562
Amodttech <aaron@amodttech.com>
2020-10-30 13:52:06 -0400

    done.

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+class Backer
+    attr_reader :name
+
+    def initialize(name)
+      @name = name
+    end
+
+    def back_project(project)
+      ProjectBacker.new(project, self)
+    end
+    
+    def backed_projects
+      new_array = ProjectBacker.all.select {|pb|pb.backer == self}
+      new_array.map {|pb| pb.project}
+    end
+  end
---
+++ lib/project.rb
@@ -0,0 +1,17 @@
+class Project
+    attr_reader :title
+
+    def initialize(title)
+      @title = title
+    end
+
+    def add_backer(backer)
+      ProjectBacker.new(self, backer)
+    end
+
+    def backers
+      new_array = ProjectBacker.all.select {|pb|pb.project == self}
+      new_array.map {|pb| pb.backer}
+    end
+    
+  end
